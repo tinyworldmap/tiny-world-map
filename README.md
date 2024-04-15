6553ba5dc6600d4ef0ea2e90875b2254f5142ca3
Bob <negenentwintig@hotmail.com>
2024-04-15 15:26:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,99 @@
+# tinyworldmap
+
+<kbd><img src="images/zoomed-out.png" /></kbd>
+<kbd><img src="images/zoomed-mid.png" /></kbd>
+<kbd><img src="images/zoomed-in.png" /></kbd>
+
+[Demo]()
+
+tinyworldmap is a world map for offline-first and low-bandwidth web apps. It's rendered client-side, and has been tested on low-end decade-old phones.
+
+It can be used with Leaflet, supports all zoom levels, and the most complete version only takes up 277 kB gzipped.
+
+The map includes the names and locations of the 10,000 most populous cities added to OpenStreetMap. At the time of writing, this means that all cities with at least 50,000 inhabitants are displayed.
+
+## Usage
+
+There are two ways the map can be used:
+
+1. As a base map, replacing OpenStreetMap tiles
+2. As an offline fallback for OpenStreetMap tiles
+
+### As a base map
+
+To use tinyworldmap as a Leaflet base map, add the following to your `head` tag:
+
+```html
+<script src="https://tinyworldmap.com/dist/tiny-world-all-10000.js">
+```
+
+This script embeds all necessary data to display the map.
+
+Then, instead of adding a tile layer to the Leaflet map, use:
+
+```js
+new L.GridLayer.TinyWorld().addTo(map)
+```
+
+The `TinyWorld` constructor takes the following options: `backgroundColor`, `textColor`, `borderColor`, `borderFillColor`, `textStrokeColor` (helps separate the text from the rest), `cityFont` (e.g. `"12px Arial"`), `countryFont`, and `dotColor`.
+
+If `dotColor` is set, every city is marked with a dot. This makes the map usable even when using a stripped-down version of tinyworldmap that doesn't include country borders:
+
+<kbd><img src="images/dotcolor.png" /></kbd>
+
+### As a fallback map
+
+In offline-first web applications, caching a map can prove to be challenging. Raster base maps consist of billions of 256x256 tiles, with a combined size of terabytes.
+
+This repository provides a [service worker](service-worker.js) to make your maps work offline.
+
+The strategy is simple: when the service worker is installed, the fallback map is preloaded and all visited pages are cached, but the base map is not. Cached data is never used while the user can reach the server. When offline, the cached pages are served. Furthermore, the service worker intercepts requests to the OSM tile server, generating replacement tiles locally using tinyworldmap.
+
+Before using this service worker, alter the sections marked `IMPORTANT`, and [register the service worker](https://web.dev/articles/service-workers-registration). Attribute OpenStreetMap and tinyworldmap in your tile layer:
+
+```js
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://www.tinyworldmap.com">tinyworldmap</a>'
+}).addTo(map);
+```
+
+## Tinier world maps
+
+The complete map is 277K gzipped/694K uncompressed. For some use cases, this is still too big.
+
+### No borders
+
+[`tiny-world-noborders-10000.js(on)`](tiny-world-noborders-10000.js) has all data in the complete version except for country borders and shorelines. This shaves off 100k from the gzipped version and 200k from the uncompressed version.
+
+Besides size, the version without borders has the advantage that all data included is precise. The country borders in the complete version are not exact at high zoom levels, which can look weird if you're overlaying shapes that match country borders and shorelines. In that case, using the version without borders often looks better.
+
+This is the default styling:
+
+<kbd><img src="images/dotcolor.png" /></kbd>
+
+### No cities
+
+If city labels are not needed, there are two versions called `tiny-world-nocities.js(on)` (which does have country labels) and `tiny-world-borders.js(on)` (which doesn't). The city labels are 410K uncompressed and 172K compressed.
+
+### Less cities
+
+Finally, for every file that does include city labels, there's a version with 2,000 cities, 4,000 cities, and 10,000 cities. Simply replace `10000` by `2000` or `4000` ion the file name.
+
+Cities included | Population
+--- | ---
+10,000 | > 48,000
+4,000 | > 137,000
+2,000 | > 287,000
+
+## Hire tinyworldmap
+
+If you require a customized map (getting a more detailed map for part of the world, translation, etc) or if you need help getting your website to work offline, contact us at info at tinyworldmap.com .
+
+## Attribution
+
+Like OpenStreetMap data, tinyworldmap data is licensed under the ODBL, which requires attribution. If you follow the steps above, you should have an attribution to both OpenStreetMap and tinyworldmap in your Leaflet footer. If not, add this:
+
+```html
+&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://www.tinyworldmap.com">tinyworldmap</a>
+```
---
+++ service-worker.js
@@ -0,0 +1,152 @@
+// Choose a cache name
+const cacheName = 'sw-v1';
+// IMPORTANT: List the files to precache
+const precacheResources = ['/', '/tiny-world-map.json'];
+// IMPORTANT: Alter the regex and tile size if you use non-standard tiles
+const REGEXP = /tile\.openstreetmap\.org\/(?<z>\d+)\/(?<x>\d+)\/(?<y>\d+)/
+const TILESIZE = 256
+
+const drawOptions = {} // see README
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+function drawPlaces(tile, coords, places, opts) {
+    var ctx = tile.getContext('2d');
+
+    let vwidth = 800, vheight = 800
+
+    var size = {x: tile.width, y: tile.height};
+    let margin1 = 2/size.x, margin2 = 35/size.x, margin3 = 50/size.x, N = Math.pow(2, coords.z);
+
+    let lbound = coords.x / N, rbound = lbound + 1/N, tbound = coords.y / N, bbound = tbound + 1/N
+
+    ctx.fillStyle = opts.backgroundColor || (path2ds.length ? "#aad3df" : 'white')
+    ctx.fillRect(0, 0, size.x, size.y)
+
+    ctx.translate(-size.x*coords.x, -size.x*coords.y)
+    ctx.scale(size.x*N/vwidth,size.y*N/vheight)
+    ctx.strokeStyle = opts.borderColor || 'black'
+    ctx.fillStyle = opts.borderFillColor || 'white'
+    ctx.lineWidth = 2/N
+
+    for (let [p, bounds] of path2ds) {
+        if (!(bounds[0] > rbound || bounds[2] < lbound || bounds[1] > bbound || bounds[3] < tbound)) {
+            ctx.fill(p)
+            ctx.stroke(p)
+        }
+    }
+
+    ctx.resetTransform()
+
+    let dotColor = opts.dotColor || (path2ds.length ? "transparent" : "red")
+    ctx.fillStyle = dotColor
+
+    if (dotColor != 'transparent')
+        for (let [yc, xc, name, zoom] of places.cities) {
+            let y = yc * N - coords.y, x = xc * N - coords.x
+            if (y > -margin1 && y < 1+margin1 && x > -margin1 && x < 1+margin1) {
+                let xS = size.x * x, yS = size.y * y
+                ctx.fillRect(xS-0,yS-0,1,1)
+            }
+        }
+
+    ctx.strokeStyle = opts.textStrokeColor || 'rgba(255,255,255,.9)'
+    ctx.lineWidth = 4
+    ctx.textAlign = 'center'
+    ctx.fillStyle = opts.textColor || "black";
+    ctx.font = opts.cityFont || '12px Arial, Helvetica, Ubuntu, sans-serif'
+    ctx.lineJoin = 'round'
+
+    for (let [yc, xc, name, zoom] of places.cities) {
+        if (zoom > coords.z) continue
+
+        let y = yc * N - coords.y, x = xc * N - coords.x
+        if (y > -margin2 && y < 1+margin2 && x > -margin2 && x < 1+margin2) {
+            let xS = size.x * x, yS = size.y * y
+            ctx.strokeText(name, xS, yS, 70)
+            ctx.fillText(name, xS, yS, 70)
+        }
+    }
+
+    ctx.font = opts.countryFont || 'bold 14px Arial, Helvetica, Ubuntu, sans-serif'
+
+    for (let [yc, xc, name, zoom] of places.countries) {
+        if (zoom > coords.z || coords.z > 8) continue
+
+        let y = yc * N - coords.y, x = xc * N - coords.x
+        if (y > -margin3 && y < 1+margin3 && x > -margin3 && x < 1+margin3) {
+            let xS = size.x * x, yS = size.y * y
+            ctx.strokeText(name, xS, yS, 100)
+            ctx.fillText(name, xS, yS, 100)
+        }
+    }
+
+    return tile;
+}
+
+
+let places
+
+async function handleTileRequest(request, match) {
+    let cache = await caches.open(cacheName)
+    // Go to the network first
+    try {
+        let response = await fetch(request.url)
+        if (!response.ok) throw new Error('No 200')
+        return response
+    }
+    // If the network is unavailable, create a replacement tile locally
+    catch(e) {
+        try {
+            places = places || await (await cache.match('/tiny-world-map.json')).json()
+            const canvas = new OffscreenCanvas(TILESIZE, TILESIZE)
+            let coords = {x: +match.groups.x, y: +match.groups.y, z: +match.groups.z}
+
+            drawPlaces(canvas, coords, places, drawOptions)
+
+            let blob = await canvas.convertToBlob({type: 'image/png'})
+
+            const headers = new Headers()
+            headers.set('content-type', blob.type)
+            headers.set('content-length', blob.size)
+            headers.set('access-control-allow-origin', '*')
+
+            return new Response(blob, {headers: headers})
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method != 'GET')
+        return
+
+    let match = REGEXP.exec(event.request.url)
+
+    if (event.request.destination === 'image' && match) {
+        event.respondWith(handleTileRequest(event.request, match))
+    }
+    else {
+        // Open the cache
+        event.respondWith(caches.open(cacheName).then((cache) => {
+            // Go to the network first
+            return fetch(event.request).then((fetchedResponse) => {
+                // IMPORTANT: Tell the service worker what not to cache
+                if (!['image', 'video', 'audio'].contains(event.request.destination))
+                    // Cache response
+                    cache.put(event.request, fetchedResponse.clone());
+
+                return fetchedResponse;
+            }).catch(() => {
+                // If the network is unavailable, try to get it from cache
+                return cache.match(event.request.url);
+            });
+        }));
+    }
+});
